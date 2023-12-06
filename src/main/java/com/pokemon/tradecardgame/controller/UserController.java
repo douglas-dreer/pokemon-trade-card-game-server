7de88dcf80e63dc01c9b850ac651fb04c7c82551
Douglas Dreer <douglas.dreer.ext@dasa.com.br>
2023-12-06 14:10:27 -0300

    Corrigido o converter para a generico.

--- src/main/java/com/pokemon/tradecardgame/controller/UserController.java
+++ src/main/java/com/pokemon/tradecardgame/controller/UserController.java
@@ -3,6 +3,7 @@ package com.pokemon.tradecardgame.controller;
 import com.pokemon.tradecardgame.entities.UserEntity;
 import com.pokemon.tradecardgame.pojo.User;
 import com.pokemon.tradecardgame.service.UserService;
+import com.pokemon.tradecardgame.utils.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.UUID;
 
-import static com.pokemon.tradecardgame.utils.Converter.convertUserEntityToUser;
-import static com.pokemon.tradecardgame.utils.Converter.convertUserToUserEntity;
+
 
 @RestController
 @RequestMapping("/users")
@@ -20,12 +20,10 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
-
     @PostMapping(produces = "application/json;charset=utf-8")
     @ResponseBody
     public ResponseEntity<User> save(@RequestBody User data,  HttpServletRequest request) {
-        UserEntity user = convertUserToUserEntity(data);
+        UserEntity user = Converter.convertTo(data, UserEntity.class);
         user = userService.save(user);
 
         URI location = URI.create(String.format("%s/%s", request.getRequestURL(), user.getId()));
@@ -38,7 +36,7 @@ public class UserController {
     @ResponseBody
     public ResponseEntity<User> findById(@PathVariable(name = "uuid") UUID uuid) {
         UserEntity userEntity = userService.findById(uuid);
-        return ResponseEntity.ok(convertUserEntityToUser(userEntity));
+        return ResponseEntity.ok(Converter.convertTo(userEntity, User.class));
     }
 
 //
